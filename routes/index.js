426204415fa8d11f5184411871870baeeacd9c83
brightmanforever022 <brightmanforever@outlook.com>
2021-01-21 14:28:56 +0800

    handle mysql disconnect

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,29 @@ const shopify = new Shopify({
   }
 });
 
-var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+var connection;
+function handleDisconnect() {
+  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 /* GET home page. */
 router.get('/', async (req, res) => {
@@ -39,7 +61,6 @@ router.get('/', async (req, res) => {
         status: results[0].status ? "started" : "stopped"
       };
       res.render('index', {page: 'index', data: settings});
-      console.log(results[0]);
     }
   })
 });
